5e3a5751f40cb4dd085afbfdeab9d156877cc4d7
tripathiravi07 <39208797+tripathiravi07@users.noreply.github.com>
2019-02-26 22:57:34 +0530

    Updated script

--- ravi/ravi.py
+++ ravi/ravi.py
@@ -1,5 +1,16 @@
 import boto3
+import click
 session=boto3.Session(profile_name='ravi')
-s3=session.resource('s3')
-for i in s3.buckets.all():
-    print(i)
+ec2=session.resource('ec2')
+@click.command()
+def list_instances():
+    "This is list of EC2 Instances"
+    for i in ec2.instances.all():
+        print(','.join((
+        i.id,
+        i.instance_type,
+        i.placement['AvailabilityZone'],
+        i.state['Name'],
+        i.public_dns_name)))
+if __name__ == '__main__':
+    list_instances()
